43b3a5213dab722765a62e9f9ef217eb59a7b49b
Sachin Ingole <97524423+sachin-rock@users.noreply.github.com>
2022-03-07 16:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express=require('express')
 const app=express()
 
 
-app.get("/user",(req,res)=>{
-    res.send("Hello")
+app.get("/libraries",checkpermisson,(req,res)=>{
+    res.send({route:"/libraries"},permission:true)
+})
+app.get("/authors",checkpermisson,(req,res)=>{
+    res.send({route:"/authors"},permission:true)
 })
 
 app.listen("5000",()=>{
